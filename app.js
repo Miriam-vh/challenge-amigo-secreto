44d27597042a1744065f9124eb5789bc21d41c0c
Miriam Vera <miriamverah57@gmail.com>
2025-09-21 17:42:37 -0600

    actualizar lista

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+//array
 let amigos = [];
 
+//Agregar amigos
 function agregarAmigo(){
     const inputAmigo = document.getElementById("amigo");
     const nombreAmigo = inputAmigo.ariaValueMax.trim();
 
+    //Campo vacio
     if(nombre === ""){
         alert("Por favor ingresa un nombre");
         return;
     }
 
+    //Nombre duplicado
     if(amigos.includes(nombreAmigo)){
         alert(`${nombreAmigo} ya esta en la lista, por favor ingresa otro nombre`);
         return;
     }
 
+    //Agregar nombre a la lista
     amigos.push(nombreAmigo);
 
+    //Limpiar campo de entrada
     inputAmigo.ariaValu = "";
-    
+
+    //Actualizar lista
+    actualizarLista();
 
 }
