73decf15eaca7c2f345699d3cbc099f275d8fb8f
Miriam Vera <miriamverah57@gmail.com>
2025-09-21 17:02:17 -0600

    agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let amigos = [];
+
+function agregarAmigo(){
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.ariaValueMax.trim();
+}
